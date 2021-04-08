2bcd921cec8b4bd7afcac5772798e541480aced8
Bruno Ferreira <marciobruno.dev@gmail.com>
2021-04-08 17:11:29 -0300

    Feito paginas web

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './global.css'
+
+import Routes from './router';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Márcio Bruno
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aperte aqui fí de Rapariga
-        </a>
-      </header>
+    <div>
+      <Routes/>
     </div>
   );
 }
