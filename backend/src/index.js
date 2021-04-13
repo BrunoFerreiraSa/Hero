e0d3e11090835d051505181e938527853cf023ac
Bruno Ferreira <marciobruno.dev@gmail.com>
2021-04-13 09:27:55 -0300

    Backend melhorado

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const {errors} = require('celebrate');
 const routers = require('./routers');
 const app = express();
 
 app.use(cors());
 app.use(express.json());  
 app.use(routers);
+app.use(errors());
 
 app.listen(3333);
 
--- backend/src/routers.js
+++ backend/src/routers.js
@@ -1,18 +1,36 @@
 const express = require('express');
+const { celebrate, Joi, Segments } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentsController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
-const SessionController = require('./controllers/SessionController')
+const SessionController = require('./controllers/SessionController');
 
 const routers = express.Router();
 
 routers.post('/sessions', SessionController.create);
 
 routers.get('/ongs', OngController.index);
-routers.post('/ongs', OngController.create);
 
-routers.get('/profile', ProfileController.index);
+routers.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    }),
+}), OngController.create);
+
+routers.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
+
+/**
+ * 18:27
+ */
 
 routers.post('/incidents', IncidentsController.create);
 routers.get('/incidents', IncidentsController.index);
