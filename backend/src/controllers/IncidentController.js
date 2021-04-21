390f4404dd6284bfe31dc46d8f4ba558bf8b1ad2
Bruno Ferreira <marciobruno.dev@gmail.com>
2021-04-21 14:29:06 -0300

    Aumento da paginação da rota

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -18,7 +18,7 @@ module.exports = {
          */
         const incidents = await connection('incidents')
          .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-         .limit(5)
+         .limit(100) //limita o numero de rotornos da Api
          .offset((page - 1) * 5)
          .select(['incidents.*', 
             'ongs.name', 
